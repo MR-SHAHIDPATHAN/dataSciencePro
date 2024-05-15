548ff65c5e71e43405b612584f36841966a7c6c8
mr-shahidpathan <shahidpathan0407@gmail.com>
2024-05-15 13:35:44 -0700

    done

---
+++ developer.py
@@ -0,0 +1,79 @@
+
+
+# #  data science 
+
+# # print function output 
+# # print('hello') # 
+
+
+# # variabls 
+
+# x = 10
+
+# ma = 26
+
+# _myage34 = 27
+
+
+# City_name = "karad"
+# print(myage + myage) 
+
+
+# myname = "dev"
+
+# print(myname) 
+
+
+
+# x = 10
+# X = 20
+# def = "karad"
+    
+
+
+
+# if(s > b){
+
+
+# }
+
+
+
+# a , b , c = "karad" , "satara",  "pune" 
+
+# x = y = z = 10
+
+
+
+
+
+
+# print(a , b , c ,x )
+
+
+# datatype 
+
+# numbers 
+# string
+# true or false 
+# 34.65
+
+
+
+a = 20  #   integer  number 
+b = 33.44 # float 
+x = "karad" # string
+ispass = True  # booleans False 
+
+# primitive data type 
+
+
+n = 33j
+print(type( ispass))
+
+
+
+
+print(a , b , x , ispass)
+
+
